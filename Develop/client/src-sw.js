a30526d31ff0485d0de9bc1f2c97f5446da2535d
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2023-01-09 16:27:48 -0800

    implemented the asset caching and plugins

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,14 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// This implements asset caching
+// We define the callback function that is going to filter the requests that we want to cache (JS and CSS files)
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [ // This plugin caches responses with the headers to 30 days max
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+}));
